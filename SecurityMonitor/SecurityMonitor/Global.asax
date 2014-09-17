b75f304dc92df4282be0f0e3176a94e3ecd9263f
Dioscar <Dioscarr@gmail.com>
2014-09-16 22:04:25 -0400

    Startup

---
+++ SecurityMonitor/SecurityMonitor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SecurityMonitor.MvcApplication" Language="C#" %>
---
+++ SecurityMonitor/SecurityMonitor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityMonitor.Startup))]
+namespace SecurityMonitor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
