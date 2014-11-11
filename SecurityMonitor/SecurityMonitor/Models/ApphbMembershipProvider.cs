50b87111271d20fce79715d56bedeecbf3792025
Dioscar <Dioscarr@gmail.com>
2014-11-11 17:06:46 -0500

    --hack--

---
+++ SecurityMonitor/SecurityMonitor/Models/ApphbMembershipProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Security;
+
+namespace SecurityMonitor.Models
+{
+    public class ApphbMembershipProvider : SqlMembershipProvider
+    {
+        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+        {
+            base.Initialize(name, config);
+
+            var connectionString = ConfigurationManager.AppSettings["DefaultConnection_Membership"];
+            FieldInfo connectionStringField = GetType().BaseType.GetField("_sqlConnectionString", BindingFlags.Instance | BindingFlags.NonPublic);
+            connectionStringField.SetValue(this, connectionString);
+        }
+    }
+    
+}
+
+ 
