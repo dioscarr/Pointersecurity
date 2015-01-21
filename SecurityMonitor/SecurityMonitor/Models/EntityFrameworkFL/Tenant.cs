04088156d9f9d5e727deafc34cf4d55fcf03cade
Dioscar <Dioscarr@gmail.com>
2015-01-20 22:49:47 -0500

    --Adding Tenant UI and delete Tenant UI--

--- SecurityMonitor/SecurityMonitor/Models/EntityFrameworkFL/Tenant.cs
+++ SecurityMonitor/SecurityMonitor/Models/EntityFrameworkFL/Tenant.cs
@@ -10,7 +10,9 @@
 namespace SecurityMonitor.Models.EntityFrameworkFL
 {
     using System;
-    using System.Collections.Generic;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
     
     public partial class Tenant
     {
@@ -20,7 +22,9 @@ namespace SecurityMonitor.Models.EntityFrameworkFL
         }
     
         public int ID { get; set; }
+        [DisplayName ("First Name")]
         public string FirstName { get; set; }
+          [DisplayName("Last Name")]
         public string LastName { get; set; }
         public string Phone { get; set; }
         public string Username { get; set; }
