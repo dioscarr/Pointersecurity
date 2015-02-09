b3d93cdd199c48db8286825672035f7997127bd5
Dioscar <Dioscarr@gmail.com>
2015-02-08 22:24:56 -0500

    --DisplayManagerRoleDelete--

---
+++ SecurityMonitor/SecurityMonitor/Models/DisplayManagerRoleDelete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SecurityMonitor.Models.EntityFrameworkFL;
+
+namespace SecurityMonitor.Models
+{
+    public class DisplayManagerRoleDelete
+    {
+        public int ID { get; set; }
+        public List<ManagerBuilding> buildingmanager{ get; set; }
+        public List<Manager> MyManagers { get; set; }
+        public List<Buildings> MyBuildings { get; set; }
+        public string other { get; set; }
+        public AspNetRoles MyRoles { get; set; }
+
+        
+    }
+}
