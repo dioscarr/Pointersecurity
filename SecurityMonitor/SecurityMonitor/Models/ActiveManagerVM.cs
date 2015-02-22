47cfe48b8007e95f4d6b3df3416fdec91b4ff1cf
dioscarr <Dioscard@hotmail.com>
2015-02-22 18:53:01 -0500

    --Activemanager --

---
+++ SecurityMonitor/SecurityMonitor/Models/ActiveManagerVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PointerSecurityAzure;
+
+namespace SecurityMonitor.Models
+{
+    public class ActiveManagerVM
+    {
+        public int Id { get; set; }
+        public string ManagerID { get; set; }
+        public int BuildingID { get; set; }
+        public Manager myManager { get; set; }
+
+        public virtual Manager Manager { get; set; }
+        public virtual Buildings Buildings { get; set; }
+    }
+}
