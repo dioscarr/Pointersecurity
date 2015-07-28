741f6e162adda9e18661023afa30ba4148bb011f
dioscarr <Dioscard@hotmail.com>
2015-07-28 16:19:49 -0400

    --updated tenant profile --

---
+++ SecurityMonitor/SecurityMonitor/Models/TenantNotification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecurityMonitor.Models
+{
+    public class TenantNotification
+    {
+        public string RequestedDate { get; set; }
+        public string TicketNumber { get; set; }
+        public string NotificationMessage { get; set; }
+    }
+}
